bec4a038ccd309749cb35ff1791a2fa69a6a6a4d
Leo <xuebin.liu@zhaopin.com.cn>
2025-07-11 18:09:59 +0800

    在 Vite 配置中添加自定义插件：实现构建时读取和修改 public/plugin.json 文件的功能，优化生成的插件 JSON 文件，提升构建过程的灵活性和可维护性。

--- 1Password-uTools/vite.config.ts
+++ 1Password-uTools/vite.config.ts
@@ -1,3 +1,5 @@
+import { readFile, writeFile } from 'node:fs/promises'
+import path from 'node:path'
 import { fileURLToPath, URL } from 'node:url'
 
 import vue from '@vitejs/plugin-vue'
@@ -7,7 +9,30 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    {
+      name: 'modify-plugin-json',
+      apply: 'build',
+      async closeBundle() {
+        // 读取 public/plugin.json
+        const srcPath = path.resolve(__dirname, 'public/plugin.json')
+        const distPath = path.resolve(__dirname, 'dist/plugin.json')
+        try {
+          let content = await readFile(srcPath, 'utf-8')
+          let json = JSON.parse(content)
+          // 在这里对 json 做你想要的修改
+          delete json.development
+          // 写入到 dist 目录
+          await writeFile(distPath, JSON.stringify(json, null, 2), 'utf-8')
+        } catch (e) {
+          console.error('处理 plugin.json 失败:', e)
+        }
+      },
+    },
+  ],
   base: './',
   resolve: {
     alias: {
